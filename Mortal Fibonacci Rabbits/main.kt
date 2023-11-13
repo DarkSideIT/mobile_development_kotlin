196284e3d3afbf7db2bb2e83f643d27b5b7ede3e
DarkSideIT <107682338+DarkSideIT@users.noreply.github.com>
2023-11-13 11:29:26 +0800

    Update main.kt

--- Mortal Fibonacci Rabbits/main.kt
+++ Mortal Fibonacci Rabbits/main.kt
@@ -1,14 +1,14 @@
-fun main() {
-    val n = 85
-    val m = 19
-    val bunnies = Array<ULong>(n) { 1u }
+fun main(){
+    a: float = readline()
+    b: float = readline()
+    m: float = readline()
+    n: float = readline()
 
-    for (month in 2 until n) {
-        bunnies[month] =
-            if (month < m) bunnies[month - 2] + bunnies[month - 1]
-            else if (month == m || month == m + 1) bunnies[month - 2] + bunnies[month - 1] - 1u
-            else bunnies[month - 2] + bunnies[month - 1] - bunnies[month - m - 1]
+
+    if ((a >= m) && (b >= n)) || ((a >= n) && (b >= m)){
+        println("YES")
     }
-
-    println(bunnies.last())
+    else {
+        println("NO")
+    }
 }
